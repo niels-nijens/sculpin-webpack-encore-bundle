79969fb4fd29f841bfeebdec078786e19b88048d
Niels Nijens <nijens.niels@gmail.com>
2019-12-27 23:45:05 +0100

    Add bundle class

---
+++ src/NijensSculpinWebpackEncoreBundle.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the SculpinWebpackEncoreBundle package.
+ *
+ * (c) Niels Nijens <nijens.niels@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Nijens\SculpinWebpackEncoreBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Registers the bundle with the kernel.
+ *
+ * @author Niels Nijens <nijens.niels@gmail.com>
+ */
+final class NijensSculpinWebpackEncoreBundle extends Bundle
+{
+}
